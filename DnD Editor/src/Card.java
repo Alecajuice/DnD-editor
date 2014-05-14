7a16cb59dc82ca713364ea1ba2bacb1aca9e7431
Alex Tarng <alecadad@gmail.com>
2014-05-13 20:35:17 -0700

    asdf

--- DnD Editor/src/Card.java
+++ DnD Editor/src/Card.java
@@ -11,7 +11,12 @@ public class Card
 		POWER, FEAT, ITEM;
 	}
 	private CardType type;
+	private BufferedImage tierImage;
+	private BufferedImage powerTypeImage;
 	private BufferedImage bgImage;
+	private BufferedImage actionTypeImage;
+	private BufferedImage rangeImage;
+
 	public Card(CardType type)
 	{
 		this.type = type;
@@ -34,11 +39,62 @@ public class Card
 		}
 	}
 
-	public CardType getType() {
+	public CardType getType()
+	{
 		return type;
 	}
 
-	public void setType(CardType type) {
+	public void setType(CardType type)
+	{
 		this.type = type;
+	}
+	public BufferedImage getTierImage()
+	{
+		return tierImage;
+	}
+
+	public void setTierImage(BufferedImage tierImage)
+	{
+		this.tierImage = tierImage;
+	}
+
+	public BufferedImage getPowerTypeImage()
+	{
+		return powerTypeImage;
+	}
+
+	public void setPowerTypeImage(BufferedImage powerTypeImage)
+	{
+		this.powerTypeImage = powerTypeImage;
+	}
+
+	public BufferedImage getBgImage()
+	{
+		return bgImage;
+	}
+
+	public void setBgImage(BufferedImage bgImage)
+	{
+		this.bgImage = bgImage;
+	}
+
+	public BufferedImage getActionTypeImage()
+	{
+		return actionTypeImage;
+	}
+
+	public void setActionTypeImage(BufferedImage actionTypeImage)
+	{
+		this.actionTypeImage = actionTypeImage;
+	}
+
+	public BufferedImage getRangeImage()
+	{
+		return rangeImage;
+	}
+
+	public void setRangeImage(BufferedImage rangeImage)
+	{
+		this.rangeImage = rangeImage;
 	}
 }
--- DnD Editor/src/CardEditorScreen.java
+++ DnD Editor/src/CardEditorScreen.java
@@ -1,22 +1,36 @@
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 import javax.swing.*;
 import javax.swing.border.*;
 
 
-public class CardEditorScreen extends JPanel
+public class CardEditorScreen extends JPanel implements ActionListener
 {
 	private Card card;
 	JPanel toolBar = new JPanel();
+	JButton save = new JButton("Save");
+	JButton back = new JButton("Back to Main Menu");
+	String[] powerTypes = {"Power Type", "At-Will", "Encounter", "Daily"};
+	JComboBox powerType = new JComboBox(powerTypes);
+	String[] tiers = {"Power Tier", "Heroic", "Paragon", "Epic"};
+	JComboBox tier = new JComboBox(tiers);
+	String[] actionTypes = {"Action Type", "Standard Action", "Minor Action", "Free Action"};
+	JComboBox actionType = new JComboBox(actionTypes);
+	String[] attackTypes = {"Power Type", "At-Will", "Encounter", "Daily"};
+	JComboBox attackType = new JComboBox(attackTypes);
+	String[] classes = {};
+	JComboBox classs = new JComboBox(classes);
 	public CardEditorScreen(Card card)
 	{
 		this.card = card;
 		this.setLayout(new BorderLayout());
 		toolBar.setBackground(Color.WHITE);
-		TitledBorder toolBarBorder = BorderFactory.createTitledBorder(BorderFactory.createEtchedBorder(EtchedBorder.LOWERED), "title");
+		TitledBorder toolBarBorder = BorderFactory.createTitledBorder(BorderFactory.createEtchedBorder(EtchedBorder.LOWERED), "Toolbar");
 		toolBarBorder.setTitleJustification(TitledBorder.RIGHT);
 		toolBar.setBorder(toolBarBorder);
-		toolBar.setLayout(new GridLayout(1, 5));
+		toolBar.setLayout(new GridLayout(2, 5));
 		switch(card.getType())
 		{
 		case POWER:
@@ -34,16 +48,51 @@ public class CardEditorScreen extends JPanel
 
 	public void setUpPowerEditor()
 	{
-		String[] powerTypes = {"Power Type", "At-Will", "Encounter", "Daily"};
-		JComboBox powerType = new JComboBox(powerTypes);
-		String[] tiers = {"Power Tier", "Heroic", "Paragon", "Epic"};
-		JComboBox tier = new JComboBox(tiers);
-		String[] actionTypes = {"Action Type", "Standard Action", "Minor Action", "Free Action"};
-		JComboBox actionType = new JComboBox(actionTypes);
-		String[] attackTypes = {"Power Type", "At-Will", "Encounter", "Daily"};
-		JComboBox attackType = new JComboBox(powerTypes);
+		toolBar.add(new JLabel());
+		toolBar.add(new JLabel());
+		toolBar.add(new JLabel());
+		toolBar.add(save);
+		toolBar.add(back);
 		toolBar.add(powerType);
 		toolBar.add(tier);
 		toolBar.add(actionType);
+		toolBar.add(attackType);
+		toolBar.add(classs);
+		powerType.addActionListener(this);
+		tier.addActionListener(this);
+		actionType.addActionListener(this);
+		attackType.addActionListener(this);
+		classs.addActionListener(this);
+		
+	}
+
+	@Override
+	public void actionPerformed(ActionEvent e)
+	{
+		JComboBox c = (JComboBox)e.getSource();
+		switch((String)c.getSelectedItem())
+		{
+		case "At-Will":
+			card.
+		}
+		
+	}
+}
+
+class CardPanel extends JPanel
+{
+	public CardPanel()
+	{
+		
+	}
+	
+	public Dimension getPreferredSize() {
+        return new Dimension(375, 525);
+    }
+	
+	
+	public void paintComponent(Graphics g)
+	{
+		super.paintComponent(g);
 	}
 }
